23de8aa53110d1235c8c545ad5380f0f502f16ee
David Ambrozio <davidambroziodev@gmail.com>
2023-12-21 16:17:33 -0300

    create servidores command

--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação linha de comando"
 	app.Usage = "Busca IP e nomes de servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "davidambrozio.dev",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPS de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "davidambrozio.dev",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome de servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -40,5 +48,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
